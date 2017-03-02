77ab2e53818351a440c3004b2db31310a24265e2
Girish Pai <girishpai@Girishs-MacBook-Pro.local>
2017-03-02 14:18:49 -0800

    Adding initial files

---
+++ model.py
@@ -0,0 +1,17 @@
+import csv
+import cv2
+
+lines = []
+with open("./data/driving_log.csv") as csvfile :
+    reader = csv.reader(csvfile)
+    for line in reader :
+        lines.append(line)
+
+images = []
+measurements = []
+
+for line in lines :
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = '../data/IMG/' + filename
+    
