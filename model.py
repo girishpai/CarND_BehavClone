30de54e64b8bed1bc2620b756f9bbb0cf2553385
Girish Pai <girishpai86@gmail.com>
2017-03-20 18:28:53 -0700

    feat : Uploading final model and video
    - Added comments in model.py

--- model.py
+++ model.py
@@ -1,15 +1,8 @@
 #This file is generated from downloading the Jupyter Notebook.
 
-# coding: utf-8
-
-# In[11]:
-
 import csv
 import cv2
 import numpy as np
-
-
-# In[12]:
 
 def open_csv(filename) :
     lines = []
@@ -19,20 +12,13 @@ def open_csv(filename) :
             lines.append(line)
     return lines
     
-
-
-# In[13]:
-
+#Read the lines from csv
 lines = open_csv("./data/new_data/driving_log.csv")
 
 
-# In[6]:
-
+#Split into train and validation samples. 
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(lines[1:], test_size=0.2)
-
-
-# In[12]:
 
 from sklearn.utils import shuffle
 def generator(samples, batch_size=32):
@@ -45,14 +31,16 @@ def generator(samples, batch_size=32):
             
             images = []
             measurements = []
+
+            #Corrections for center, right and left images
             corrections = [0,0.3,-0.3]
+
             for line in batch_samples :
                 for i in range(0,3) :
                     source_path = line[i]
                     correction  = corrections[i]
                     filename = source_path.split('/')[-1]
                     current_path = './data/new_data/IMG/' + filename
-                    #print(current_path + '\n')
                     image = cv2.imread(current_path)
                     images.append(image)
                     measurement = float(line[3]) + correction
@@ -60,22 +48,21 @@ def generator(samples, batch_size=32):
                     images.append(cv2.flip(image,1))
                     measurement = float(line[3]) + correction
                     measurements.append(measurement * -1.0)
-                    
-            # trim image to only see section with road
+
             X_train = np.array(images)
             y_train = np.array(measurements)
-            #print(X_train.shape)
+
             yield shuffle(X_train, y_train)
 
 
-# In[13]:
+
 
 # compile and train the model using the generator function
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
 
-# In[14]:
+
 
 from keras.models import Sequential
 from keras.layers import Dense, Flatten,Lambda,Cropping2D
@@ -83,7 +70,7 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.core import Dropout
 
-# In[15]:
+
 
 # Architecture used by NVIDIA in the following paper.
 #https://arxiv.org/pdf/1604.07316.pdf
@@ -104,15 +91,15 @@ model.add(Dense(1))
 model.compile(loss='mse',optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch=
             len(train_samples)* 4, validation_data=validation_generator,
-                    nb_val_samples=len(validation_samples), nb_epoch=1)
+                    nb_val_samples=len(validation_samples), nb_epoch=6)
 
 
-# In[16]:
 
-model.save('model_temp.h5')
+#Saving the model
+model.save('model.h5')
 
 
-# In[ ]:
+
 
 
 
